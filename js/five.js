4f08a9ff403e8bdf9f3576ff2a17c842a800600c
impressivecoder1 <rohanrahman251@gmail.com>
2025-11-05 04:00:24 +0600

    five

--- js/five.js
+++ js/five.js
@@ -1,1 +1,12 @@
 console.log('comment box')
+//  document.getElementById('post-comment-btn').addEventListener('click', function(){
+//             const commentBox = document.getElementById('comment-box-textarea');
+//             const reserve = commentBox.value;
+//             // console.log(reserve)
+//             const commentContainer = document.getElementById('comment-container');
+//             // console.log(commentContainer)
+//             const commentElement = document.createElement('p');
+//             commentElement.innerText = reserve;
+//             commentContainer.appendChild(commentElement).classList.add('comment')
+//             commentBox.value = '';
+//         })
